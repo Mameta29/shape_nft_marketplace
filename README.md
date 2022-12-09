d3ed239e826c07916d566a78c4de4c07e0ea3f73
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-12-09 23:28:10 +0900

    テストコード追加

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## How to start
 
-1. create .env file
+1. create .env file in backend directory & .env.local file in frontend directory
 2. enter your APIKey & privatekey
 
 ```zsh
@@ -10,16 +10,25 @@ ALCHEMY_GOERLI_URL=YOUR_DATA
 PRIVATE_KEY=YOUR_DATA
 ```
 
+```zsh
+NEXT_PUBLIC_PROJECT_ID=YOUR_DATA
+NEXT_PUBLIC_PROJECT_SECRET=YOUR_DATA
+```
+
 3. install modules
 
 ```zsh
-npm i
+cd frontend && npm i
+```
+
+```zsh
+cd backend && npm i
 ```
 
 4. deploy contract
 
 ```zsh
-npm run deploy:contract
+cd backend && npm run deploy
 ```
 
 result example
@@ -33,5 +42,5 @@ NFTMarket deployed to: 0x149920786500a12dA84185df4B4aAABe975Df5f8
 5. start frontend
 
 ```zsh
-yarn dev
+cd frontend && yarn dev
 ```
--- backend/test/NFTMarket.test.js
+++ backend/test/NFTMarket.test.js
@@ -8,6 +8,7 @@ describe('NFTMarketplace', function () {
       //　テスト用の変数
       const NAME ='Shape Market';
       const SYMBOL = 'SHP';
+      const BASE_URL = 'ipfs://bafybeihcyruaeza7uyjd6ugicbcrqumejf6uf353e5etdkhotqffwtguva';
 
       /**
        * deploy function
@@ -35,11 +36,74 @@ describe('NFTMarketplace', function () {
                   const { market } = await loadFixture(deployContract);
                   // get listprice
                   var price = await market.getListingPrice();
-                  console.log("price:", price);
+                  // console.log("price:", Number(price));
                   // 名前とシンボル、リストした価格などの情報を確認
                   expect(await market.name()).to.eql(NAME);
                   expect(await market.symbol()).to.eql(SYMBOL);
-                  // expect(await market.getListingPrice()).to.eql(BigNumber.from(0.025));
+                  expect(Number(price)).to.eql(Number(web3.utils.toWei('0.025')));
             });
-        });
+
+            it("get init nfts", async function () {
+                  // コントラクトをデプロイ
+                  const { market } = await loadFixture(deployContract);
+                  // get market items (この時点では初期段階なので空)
+                  const items = await market.fetchMarketItems();
+                  // check
+                  expect(items).to.eql([]);
+            });
+
+            it("get init fetch nfts", async function () {
+                  // コントラクトをデプロイ
+                  const { market } = await loadFixture(deployContract);
+                  // get market items (この時点では初期段階なので空)
+                  const items = await market.fetchMyNFTs();
+                  // check
+                  expect(items).to.eql([]);
+            });
+
+            it("get init listed nfts", async function () {
+                  // コントラクトをデプロイ
+                  const { market } = await loadFixture(deployContract);
+                  // get market items (この時点では初期段階なので空)
+                  const items = await market.fetchItemsListed();
+                  // check
+                  expect(items).to.eql([]);
+            });
+      });
+
+      /**
+       * change price
+       */
+      describe("change price", function () {
+            it("change listPrice test", async function () {
+                  // コントラクトをデプロイ
+                  const { market } = await loadFixture(deployContract);
+                  // 更新後のprice
+                  const newPrice = web3.utils.toWei('0.029');
+                  // change
+                  await market.updateListingPrice(newPrice);
+                  // get listprice
+                  var price = await market.getListingPrice();
+                  // check
+                  expect(Number(price)).to.eql(Number(newPrice));
+            });
+      });
+
+      /**
+       * create Token test code
+       */
+      describe("create token", function () {
+            it("create token test", async function () {
+                  // コントラクトをデプロイ
+                  const { market, owner} = await loadFixture(deployContract);
+                  // price
+                  const price = web3.utils.toWei('0.025');
+                  // create token
+                  const id = await market.createToken(BASE_URL, price, owner.getAddress(), true, {
+                        value: price
+                  });
+                  // check
+                  expect(id).to.eql(1);
+            });
+      });
 });
